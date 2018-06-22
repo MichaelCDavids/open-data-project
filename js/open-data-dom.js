b5cb17dcc74b853b0632248a49e989c840d073af
Michael Davids <michaelcdavids@gmail.com>
2018-06-22 08:01:03 +0200

    adding the DOM code

--- js/open-data-dom.js
+++ js/open-data-dom.js
@@ -1,9 +1,30 @@
-// References to do elements
+// References to DOM elements
+var ageSelector = document.querySelector(".age");
+var genderSelector = document.querySelector(".gender");
+var areaSelector = document.querySelector(".area");
+var doingSelector = document.querySelector(".doing");
+var searchButton = document.querySelector("#search-button");
 
-//Handlebars Templates
+//Template for search results
+var templateSource = document.querySelector(".afterSearchTemplate").innerHTML;
+var templateSearchedPeople = Handlebars.compile(templateSource);
+var insertSearchDataElement = document.querySelector(".insertSearchDataElement");
+
+//Template for area list
+var templateSource = document.querySelector(".areaListTemplate").innerHTML;
+var templateAreaList = Handlebars.compile(templateSource);
+var insertAreaListElement = document.querySelector(".insertAreaListElement");
 
 //Instance of Factory function
+var factory = OpenDataFactory(people);
 
-// DOM Functions
+//on page load event
+window.addEventListener('load',function(){
+   areaSelector.innerHTML = templateAreaList({areas : factory.getAreaList()});
+});
 
 // Event Listeners
+searchButton.addEventListener('click',function(){
+  console.log(factory.search(ageSelector.value,genderSelector.value,areaSelector.value,doingSelector.value));
+     insertSearchDataElement.innerHTML = templateSearchedPeople({searchedPeople : factory.search(ageSelector.value,genderSelector.value,areaSelector.value,doingSelector.value)});
+});
